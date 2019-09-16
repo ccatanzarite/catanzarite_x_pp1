2f9fc5f439abc361cfe72e3e98b01685c7893dac
Clayton Catanzarite <ccatanzarite@bellarmine.edu>
2019-09-15 22:45:06 -0400

    minor adjustments to all files

--- Application.java
+++ Application.java
@@ -6,26 +6,40 @@
 *CS322 - Compiler Construction
 *Fall 2019
 */
-import java.util.Scanner;
-import javax.swing.*;
-import java.io.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 //import javax.awt.*;
 public class Application extends Regex{
 	public static void main(String args[]) throws FileNotFoundException {
-		//Jframe frame = new Jframe("Application");
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//frame.setSize(300,300);
-		//frame.setVisible(true);
+		Jframe frame = new Jframe("Application");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,300);
+		frame.setVisible(true);
 		Scanner scan=new Scanner(System.in);
-		File log=new File(auth.log);
+		File log=new File(auth.log);//TODO:figure out how to add file
 		Scanner scnr = new Scanner(log);
 		String line ="";
-		hashMapCreator
-		while(scnr.nextLine())
-			line=scnr.nextLine;
+		hashMapCreator();
+		while(scnr.nextLine()){
+			line=scnr.nextLine();
 			hashMapEntry(line);
+		}//while loop
 	}//main method
-	/**
+	
 	public static void  argumentAccepter(){
 		System.out.println("enter filename\n");
 		String filename = scan.nextLine();
@@ -35,5 +49,5 @@ public class Application extends Regex{
 		System.out.println("The print flag is "+printFlag);
 		 
 	}//argument method
-	*/
+	
 }//class
--- Regex.java
+++ Regex.java
@@ -6,17 +6,38 @@
 *CS322 - Compiler Contrusction
 *Spring 2019
 */
-import java.util.*;
+import java.util.Scanner;
+import java.util.regex.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
 public class Regex{
 	public static void hashMapCreator() throws FileNotFoundException {
-		HashMap<String,Integer> hm= new HashMap<String,Integer>();
-		
+		HashMap<String,Integer> hmIP= new HashMap<String, Integer>();
+		HashMap<String,Integer> hmUser= new HashMap<String, Integer>();
 
 	}//hashMapCreator
-	public static void hashMapEntry(String str) throws FileNotFoundException{
-		if 
+	public static void hashMapIpEntry(String str) throws FileNotFoundException{
+		String IPADDRESS_PATTERN = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+		Pattern pattern=Pattern.compile(IPADDRESS_PATTERN);
+		Matcher matcher=pattern.matcher(str);
+		if(matcher.find()){
+			String matchedIP=matcher.group();
+			hmIP.put(matchedIP,1);
+			
+		}//if ip is found
 	}//hashMapEntry
+	public static void hashMapUserEntry(String strUser) throws FileNotFoundException{
+		
+	}//hashMapUserEntry
+	public static int hashMapSize(){
+		return hmIP.size();
+	}//hashMapSize
+	public static void hashMapcontents(){
+		for(int i=0;i<=hmIP.size();i++)
+			System.out.println(Arrays.asList(hmIP));
+	}//hashmap contents
 
 }//class
